443db897c683f573e06a3e80080b37948e6b4237
pavan-kumar-99 <pavan1999.kumar@gmail.com>
2021-12-29 16:39:57 +0530

    Added Component files to get from Raw URL

---
+++ components/getDataFromRawUri.py
@@ -0,0 +1,14 @@
+import kfp
+import kfp.dsl as dsl
+import kfp.components as components
+from kfp.components import create_component_from_func
+
+
+@components.create_component_from_func
+def GetRawData(val: str = 'https://raw.githubusercontent.com/srafay/Machine_Learning_A-Z/master/Part%202%20-%20Regression/Section%204%20-%20Simple%20Linear%20Regression/Salary_Data.csv') -> str:
+    import requests
+    data = requests.get(val)
+    return data
+   
+if __name__ == '__main__':
+    create_component_from_func(func=GetRawData,base_image='python:slim',output_component_file='getRawData.yaml')
